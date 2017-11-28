cf4bcae587663e352e58a61355b33107e3c0ce77
hsong1 <hsong56@wisc.edu>
2017-11-29 00:15:38 +0900

    update simulated data

---
+++ R/simulPU.R
@@ -0,0 +1,10 @@
+#' simulated PU data
+#'
+#' A simulated data for the illustration. 
+#' we draw covariates \eqn{x_i} from \eqn{N(\mu,I_{5x5})} or \eqn{N(-\mu,I_{5x5})} with probability 0.5.
+#' To make the first two variables active, we let \eqn{\mu = [\mu_1,\dots,\mu_2,0,0,0]^T, \theta = [\theta_0,\dots,\theta_2,0,0,0]^T} and set \eqn{\mu_i=1.5, \theta_i ~ Unif[0.5,1]}
+#' Responses \eqn{y_i} is simulated via \eqn{P_\theta(y=1|x) = 1/exp(-\theta^Tx)}.
+#' 1000 observations are sampled from the subpopulation of positives(y=1) and labelled, and another 1000 observations are sampled from the original population and unlabelled.
+#'
+#' @format A list containing model matrix X, true response y, labelled/unlabelled response vector z, and a true positive probability truePY1. 
+"simulPU"
--- R/simulatedPUdata.R
+++
@@ -1,6 +0,0 @@
-#' simulated PU data
-#'
-#' A simulated data for the illustration. The first two columns X1-X2 are active variables. 1000 observations are sampled from the positives and labelled, and another 1000 observations are sampled from the population and unlabelled.
-#'
-#' @format A list containing model matrix X, response vector z, and a true positive probability truePrevalence. 
-"simulatedPUdata"
