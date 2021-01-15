6cc6411c14fc7a13f015a26921c8b12b5ada2016
hsong1 <hsong56@wisc.edu>
2021-01-15 16:18:06 -0500

    cran update

--- R/PUlasso.R
+++ R/PUlasso.R
@@ -2,8 +2,8 @@
 #' @description The package efficiently solves PU problem in low or high dimensional setting using Maximization-Minorization and (block) coordinate descent. It allows simultaneous feature selection and parameter estimation for classification. Sparse calculation and parallel computing are supported for the further computational speed-up. See Hyebin Song, Garvesh Raskutti (2018) <\url{https://arxiv.org/abs/1711.08129}>.
 #' @details
 #' Main functions: grpPUlasso, cv.grpPUlasso, coef, predict
-#' @author Hyebin Song, \email{hsong@@stat.wisc.edu}, Garvesh Raskutti, \email{raskutti@@stat.wisc.edu}.
-#' @keywords PUlearning, Lasso, Group Lasso
+#' @author Hyebin Song, \email{hps5320@@psu.edu}, Garvesh Raskutti, \email{raskutti@@stat.wisc.edu}.
+#' @concept PUlearning Lasso GroupLasso
 #' @examples
 #' data("simulPU")
 #' fit<-grpPUlasso(X=simulPU$X,z=simulPU$z,py1=simulPU$truePY1)
